3c9919f45d68edeb4682427b182791b00a1fa618
Clay <claymcintosh@gmail.com>
2022-01-19 09:16:51 +1000

    Adding basic API variables for later use

--- javascript.js
+++ javascript.js
@@ -1,1 +1,9 @@
 var APIKey = "6dfd8cecea8d0220febc6bc4c30dbd34";
+var city;
+var queryURL =
+  "http://api.openweathermap.org/data/2.5/weather?q=" +
+  city +
+  "&appid=" +
+  APIKey;
+
+fetch(queryURL);
