81b6083fb49103f7c2c1ce23646f838a777371f4
Clay <claymcintosh@gmail.com>
2022-01-19 09:46:07 +1000

    wrote some psudocode and restructured my folder

---
+++ Assets/javascript.js
@@ -0,0 +1,33 @@
+var APIKey = "6dfd8cecea8d0220febc6bc4c30dbd34";
+var city;
+var queryURL =
+  "http://api.openweathermap.org/data/2.5/weather?q=" +
+  city +
+  "&appid=" +
+  APIKey;
+
+fetch(queryURL);
+
+//retreve localStorage of stored cities
+
+//on search, store city to localStorage
+
+//when user searches for a city, it updates to that city
+
+//when user searches a city, find the city (longitude and latitude?)
+
+//provide the city name
+
+//provide the date
+
+//provide an icon representing weather condition
+
+//provide temperature
+
+//provide UV index by colour (favourable, moderate, severe)
+
+//5day forcast with date
+
+//5day forcast with icon of weather conditions
+
+//when user clicks on a city in search history, it updates to that city
